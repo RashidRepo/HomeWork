ce2df89a1e07a86570ee57a74644cf5237d0177a
icsbcloud <icsbmac@icloud.com>
2024-05-26 10:37:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.homework
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.homework.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val CHANNEL_ID = "example_channel_id"
+        const val NOTIFICATION_ID = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.snack.setOnClickListener {
+            Snackbar.make(binding.root, "This is snackbar", Snackbar.LENGTH_LONG).setAction("OK"){}.setActionTextColor(
+                Color.GREEN).show()
+        }
+
+        binding.alert.setOnClickListener {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Save")
+            alert.setMessage("Sure to save?")
+            alert.setPositiveButton("Yes") { dialog, which ->
+                Snackbar.make(binding.root, "Saved!", Snackbar.LENGTH_LONG).show()
+            }
+            alert.setNegativeButton("No") { dialog, which ->
+                dialog.dismiss()
+            }
+            alert.setNeutralButton("Remind Later", null)
+            alert.show()
+        }
+
+        createNotificationChannel()
+
+        binding.notif.setOnClickListener {
+            showNotification()
+        }
+
+
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Example Channel"
+            val descriptionText = "This is an example notification channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showNotification() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(androidx.core.R.drawable.notification_bg)
+            .setContentTitle("Example Notification")
+            .setContentText("This is a notification example.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+}
